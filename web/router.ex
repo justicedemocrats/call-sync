f7a7b259fc3a908a89e1e4ed8996cfd7d84ec028
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-01-20 16:42:10 -0500

    use secrets to keep people out

--- web/router.ex
+++ web/router.ex
@@ -1,8 +1,14 @@
 defmodule CallSync.Router do
   use CallSync.Web, :router
 
+  pipeline :main do
+    plug(CallSync.SecretPlug)
+  end
+
   scope "/", CallSync do
+    pipe_through :main
+
     get("/", IndexController, :index)
-    get("/question-lookup/:slug", IndexController, :question_lookup)
+    get("/configure/:slug", IndexController, :question_lookup)
   end
 end
