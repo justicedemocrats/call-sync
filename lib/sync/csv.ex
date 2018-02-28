18bbd2609ed0e4d3a733131197dd3e2ce3db6a0a
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-02-28 14:08:13 -0500

    slow it down a bit

--- lib/sync/csv.ex
+++ lib/sync/csv.ex
@@ -15,7 +15,7 @@ defmodule Sync.Csv do
       results_stream
       |> Stream.with_index()
       # |> Flow.from_enumerable(min_demand: 50, max_demand: 100)
-      |> Flow.from_enumerable(min_demand: 25, max_demand: 50)
+      |> Flow.from_enumerable(min_demand: 15, max_demand: 25)
       |> Flow.filter(fn {call, idx} -> {should_sync(call, config), idx} end)
       |> Flow.map(fn {call, idx} ->
         if rem(idx, @print_interval) == 0 do
