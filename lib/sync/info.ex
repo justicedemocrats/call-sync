a01830ded361302546ca39ec783ddbba7ac88c12
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-03-02 08:21:31 -0500

    24 hrs

--- lib/sync/info.ex
+++ lib/sync/info.ex
@@ -88,9 +88,9 @@ defmodule Sync.Info do
   end
 
   def within_24_hours do
-    ago = Timex.shift(Timex.now(), hours: -48)
+    # ago = Timex.shift(Timex.now(), hours: -48)
     up_to = Timex.shift(Timex.now(), hours: -2)
-    # ago = Timex.shift(Timex.now(), hours: -24)
+    ago = Timex.shift(Timex.now(), hours: -24)
     %{"timestamp" => %{"$gt" => ago, "$lt" => up_to}}
   end
 
